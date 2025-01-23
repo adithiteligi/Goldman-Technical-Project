219a49a1a6aad8d6ac6446ffa1ba10b204a264eb
carterboyce <cboyceuse@gmail.com>
2025-01-23 18:27:43 -0500

    clarifying updates w/ syntax and no error block

--- mutual-fund-10/frontend/connect.js
+++ mutual-fund-10/frontend/connect.js
@@ -6,13 +6,25 @@ function requestcalculatereturn() { //function requesting on flask server
     // beta not needed 
     const url = `http://127.0.0.1:5000/calculate?principal_amount=${principal_amount}&ticker=${ticker}&num_years=${num_years}`;
     // url that I used from pycalc.py, kind of like f string
+    
+    axios.get(url)
+    .then(response => {
+        // Only check for investment_return and display it
+        if (response.data.investment_return) {
+            document.getElementById("result").innerText = `Investment Return: ${response.data.investment_return}`;
+        }
+    });
 
+    // The following axios request is commented out
+    /*
     axios.get(url)
         .then(response => {
             if (response.data.investment_return) {
-                document.getElementById("result").innerText = `Investment Return: ${response.data.investment_return}`;
+                document.getElementById("Investment Result Placement").innerText = `Investment Return: ${response.data.investment_return}`;
             } else if (response.data.error) {
-                document.getElementById("result").innerText = `Error: ${response.data.error}`;
+                document.getElementById("Investment Result Placement").innerText = `Error: ${response.data.error}`;
             }
         });
+    */
 }
+        
